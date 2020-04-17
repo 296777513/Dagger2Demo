39d344669655de88a65541136b408ad16273e711
yachao_li <yachao.li@airbnb.com>
2020-04-17 11:13:42 +0800

    Replace the directory of META-INFO with AutoService

--- processor/crossguard/src/main/java/com/example/knight/dagger2/DaggerProcessor.kt
+++ processor/crossguard/src/main/java/com/example/knight/dagger2/DaggerProcessor.kt
@@ -2,26 +2,18 @@ package com.example.knight.dagger2
 
 import com.example.knight.dagger.*
 import com.example.knight.dagger2.componentprocessor.ComponentProcessor
-import com.example.knight.dagger2.componentprocessor.ComponentWriter
-import com.example.knight.dagger2.subcomponentprocessor.SubComponentModel
 import com.example.knight.dagger2.subcomponentprocessor.SubComponentProcessor
-import com.example.knight.dagger2.subcomponentprocessor.SubComponentWriter
-import com.squareup.javapoet.JavaFile
-import com.squareup.javapoet.MethodSpec
-import com.squareup.javapoet.TypeSpec
+import com.google.auto.service.AutoService
 import me.eugeniomarletti.kotlin.processing.KotlinAbstractProcessor
-import java.io.IOException
-import java.util.*
-import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.Processor
 import javax.annotation.processing.RoundEnvironment
 import javax.annotation.processing.SupportedSourceVersion
 import javax.lang.model.SourceVersion
-import javax.lang.model.element.Modifier
 import javax.lang.model.element.TypeElement
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
+@AutoService(Processor::class)
 class DaggerProcessor : KotlinAbstractProcessor() {
-
 
     private val usdAnnotations: Set<Class<out Annotation>> = setOf(
         AppGraphDeclaration::class.java,
